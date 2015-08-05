26f844b4428aca2dfbf5aaf5a0abc140c83a0818
evan-Law <evanLaw@luoxis-MacBook-Air.local>
2015-08-05 22:35:34 +0800

    add notification items

--- mainPage/recommend.php
+++ mainPage/recommend.php
@@ -1,9 +1,10 @@
 <?php  
   
-header(‘Content-type:text/json’);     //这句是重点，它告诉接收数据的对象此页面输出的是json数据；  
+header('Content-type:text/json');     //这句是重点，它告诉接收数据的对象此页面输出的是json数据；  
   
-$json={“n”:”name”,”p”:”password”};           //虽然这行数据形式上是json格式，如果没有上面那句的话，它是不会被当做json格式的数据被处理的；  
+$json={"n":"name","p":"password"};           //虽然这行数据形式上是json格式，如果没有上面那句的话，它是不会被当做json格式的数据被处理的；  
   
-echo $json;  
+  echo json_encode($json);
+#echo $json;  
   
 ?>
